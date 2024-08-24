c16edad28529c56fac38ff7caf76cb5b21cb1597
Jewel Esquilona <144073924+JewelEsquilona@users.noreply.github.com>
2024-08-25 00:56:57 +0800

    Add files via upload

---
+++ grp4_esquilona_balasta_malones_recreo_pili_ex1.py
@@ -0,0 +1,193 @@
+fullname = "Jewel Esquilona" 
+course = "BSCS"
+year = 3
+section = "G"
+print(f"Hello, I'm {fullname}! {course} {year}{section}")
+
+# Exercise 1
+print("Exercise 1")
+# My first simple script
+
+number = 4 
+name = "Group"  
+print("Name:", name)
+print("Number:", number)
+
+mem1 = "Kyla"
+mem2 = "Trisha"
+mem3 = "Jenny"
+mem4 = "Jayson"
+print("Group Members:", mem1 , mem2 , mem3, mem4)
+
+sun, snow, rain = "Flower" , "Man" , "Drop" 
+print("Sun:", sun, "Snow:", snow, "Rain:", rain)
+
+hobby = passion = interest = "Gaming"
+print("Hobby:", hobby, "Passion:", passion, "Interest:", interest)
+
+
+
+
+#Create a Simple Program with Fullname, Course, Year, and Section
+ 
+
+# My first simple script
+print("Hello, I'm Kyla Malones! BSCS 3G")
+
+ 
+#Python Variables
+
+# Create two variables and assign number and name on it
+number = 22
+name = "Kyla Malones"
+
+# Then print both values
+print("Number:", 22)
+print("Name:","Kyla Malones")
+
+ 
+#Python - Variable Names
+
+# Create 4 variable names using the first names of your group members
+
+"Jewel"
+"Jayson"
+"Jenny"
+"Trisha"
+
+# Print the variables to verify
+print("Jewel, Jayson, Jenny, Trisha")
+
+ 
+#Many Values to Multiple Variables
+# Assign values to multiple variables in one line
+a, b, c = 5, 10, 15
+
+# Print the variables to verify
+print(a, b, c)
+
+# One Value to Multiple Variables
+# Assign the same value to multiple variables in one line
+x = y = z = "Same Value"
+
+# Print the variables to verify
+print(x, y, z)
+
+
+
+
+
+# a. Create a simple print script
+print("Goodmorning, I'm Jenny Balasta! BSCS 3G")
+
+# b. Add comment "My first simple script"
+# My first simple script
+
+# Python Variables
+# a. Create two variables and assign number and name to it
+number = 25
+name = "Jenny"
+
+# b. Print both values
+print("Number:", number)
+print("Name:", name)
+
+# Python - Variable Names
+# a. Create 4 variable names using the first names of your group members (excluding your name)
+jewel = "Jewel"
+trisha = "Trisha"
+kyla = "Kyla"
+jayson = "Jayson"
+
+# Many Values to Multiple Variables
+# a. Assign values to multiple variables in one line
+x, y, z = 5, 10, 15
+
+# One Value to Multiple Variables
+# a. Assign the same value to multiple variables in one line
+a = b = c = 50
+
+
+
+
+
+# Comments
+# a. Create a simple print script
+# b. Add comment "Introduction Script"
+
+# Introduction Script
+print("Hello, I'm Trisha Recreo! BSCS 3G")
+
+# Variables
+# a. Create two variables and assign number and name on it.
+age = 21
+student_name = "Trisha Recreo"
+
+# b. Then print both values
+print("Age:", age)
+print("Student Name:", student_name)
+
+# Variable Names
+# a. Create 4 variable names using the first names of your group members.
+Jewel = "Jewel"
+Jayson= "Jayson"
+Kyla = "Kyla"
+Jenny = "Jenny"
+
+# Print the values of the group member variables
+print("Group Member Names:")
+print("Jewel:",Jewel)
+print("Jayson:",Jayson)
+print("Kyla:",Kyla)
+print("Jenny:", Jenny)
+
+# Many Values to Multiple Variables
+# a. Assign values to multiple variables in one line
+length, width, height = 200, 100, 50
+print("Length:", length)
+print("Width:", width)
+print("Height:", height)
+
+# One Value to Multiple Variables
+# a. Assign the same value to multiple variables in one line
+hobby1 = hobby2 = hobby3 = "Playing Basketball"
+print("Common Hobbies:", hobby1, hobby2, hobby3)
+
+
+
+#Create a simple program with your fullname, course, year and section 
+fullname = "Jayson Pili"
+course = "BSCS"
+year = 3
+section = "G"
+print(f"Hello, I'm {fullname}! {course} {year}{section}")
+
+# Python Comments
+# My First simple script
+print("Jayson Pili\n")
+
+#Python Variables
+age = 23
+course = "Computer Science\n"
+print(age)
+print(course)
+
+#Python - Variable Names
+trisha = 21
+jenny = 22
+jewel = 21
+kyla = 24
+
+print (trisha)
+print (jenny)
+print (jewel)
+print (kyla)
+
+#Many Values to Multiple Variables
+jayson, travis_cat , lebron = "\npogi", "pusa", "kambing"
+print(jayson,travis_cat,lebron)
+
+#One Values to Multiple Variables
+jayson = travis_cat = travis_pusa = "\ncute na pusa"
+print (jayson, travis_cat,travis_pusa)
+
