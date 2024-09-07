675a542a98b5b657d69a509cb33069a4de2f2822
Jewel Esquilona <144073924+JewelEsquilona@users.noreply.github.com>
2024-09-07 18:58:52 +0800

    Add files via upload

---
+++ grp4_esquilona_ex1.py
@@ -0,0 +1,26 @@
+fullname = "Jewel Esquilona" 
+course = "BSCS"
+year = 3
+section = "G"
+print(f"Hello, I'm {fullname}! {course} {year}{section}")
+
+# Exercise 1
+print("Exercise 1")
+# My first simple script
+
+number = 4 
+name = "Group"  
+print("Name:", name)
+print("Number:", number)
+
+mem1 = "Kyla"
+mem2 = "Trisha"
+mem3 = "Jenny"
+mem4 = "Jayson"
+print("Group Members:", mem1 , mem2 , mem3, mem4)
+
+sun, snow, rain = "Flower" , "Man" , "Drop" 
+print("Sun:", sun, "Snow:", snow, "Rain:", rain)
+
+hobby = passion = interest = "Gaming"
+print("Hobby:", hobby, "Passion:", passion, "Interest:", interest)
